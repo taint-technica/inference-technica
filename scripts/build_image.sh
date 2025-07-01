29638561bc2b3203ca9958928edc884df299564e
taint-technica <taint@technica.ai>
2025-07-01 07:49:42 +0000

    Add build and run scripts for Docker image and container management
    - Introduced `build_image.sh` to streamline Docker image creation.
    - Added `run_container.sh` for launching the Docker container with GPU support.
    - Updated Dockerfile to remove the CUDA keyring package after installation and adjusted the installation path for dependencies.

---
+++ scripts/build_image.sh
@@ -0,0 +1,1 @@
+docker build -f ./xinference/deploy/docker/Dockerfile -t xinference:latest .
---
+++ scripts/run_container.sh
@@ -0,0 +1,1 @@
+docker run --gpus all --name xinference -p 9997:9997 --entrypoint tail -d -v "$(pwd)":/opt/inference xinference:latest -f /dev/null
